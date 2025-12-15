9f666347c0e95a523bb71629bfb5612b88804872
Facundo  Bravo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-12-15 23:17:11 +0000

    implementacion: sistema de guardado de imagenes en form de signup

--- src/front/components/authentication/Signup.jsx
+++ src/front/components/authentication/Signup.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Swal from 'sweetalert2'
 import { fetchSignup } from "../../services/authServices"
 import { useNavigate } from "react-router-dom"
+import { BACKEND_URL } from "../../main"
 
 
 export const Signup = () => {
@@ -22,8 +23,30 @@ export const Signup = () => {
         }
     )
 
-    const handleProfilePhoto = (event) =>{
-        console.log(event)
+    const uploadToCloudinary = async(file) => {
+        const cloudinaryForm = new FormData()
+        cloudinaryForm.append("image", file)
+
+
+        const response = await fetch(`${BACKEND_URL}api/upload-image`,{
+            method : "POST",
+            body: cloudinaryForm
+        })
+
+        const data = await response.json()
+
+
+        return data.secure_url
+
+
+    }
+
+    const handleProfilePhoto = async(event) =>{
+        const file = event.target.files[0]
+       
+        const image = await uploadToCloudinary(file)
+        setFormData({...formData, profile_photo: image})
+        setProfilePhoto(image)
     }
 
 
@@ -100,6 +123,13 @@ export const Signup = () => {
     return (
       
         <form onSubmit={handleSubmit}>
+            {profilePhoto &&
+            <div style={{width:"20rem"}}>
+                <div className="ratio ratio-1x1">
+                <img className="object-fit-cover" src={profilePhoto} alt="profilePhoto" />
+                </div>
+            </div>
+            }
             <label>Foto de perfil</label>
             <input  onChange={handleProfilePhoto} type="file" ></input>
             <label>Nombre marinero</label>
